9934346eb6a458b530670671b075d1c40cc666e0
knifeAn <32318545+knifeAn@users.noreply.github.com>
2020-03-24 13:47:48 +0800

    Update start.sh

--- deployment/script/start.sh
+++ deployment/script/start.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
-cd /home/anxiaodong/jenkins
+while true
+do
+    echo "运行正常"
+    sleep 10
+done
+
+#cd /home/anxiaodong/jenkins
 #docker run --name beehello -p 8083:81 -d beehello:$VERSION
-./beehello
+#./beehello
