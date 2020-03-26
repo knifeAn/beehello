4c39f5b0748f5c9ded7ca78e09691ebfdba5f821
knifeAn <32318545+knifeAn@users.noreply.github.com>
2020-03-26 09:43:05 +0800

    Update start.sh

--- deployment/script/start.sh
+++ deployment/script/start.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
+mkdir /lib64 && ln -s /lib/libc.musl-x86_64.so.1 /lib64/ld-linux-x86-64.so.2
 ./beehello
 while true
 do
     echo "运行正常"
-    sleep 10
+    sleep 10m
 done
 
 #cd /home/anxiaodong/jenkins
